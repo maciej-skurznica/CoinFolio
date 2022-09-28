eaf376c64d80696b6b68fdf0182c86fb1a83df5b
Maciej Skurznica <maciej.skurznica@gmail.com>
2022-09-28 22:43:39 +0100

    CF-23 address comments

--- src/components/Table/Table.js
+++ src/components/Table/Table.js
@@ -9,40 +9,36 @@ class Table extends React.Component {
   state = {
     isLoading: true,
     coinsData: [],
-    numberOfCoinsToLoad: 50,
-    allCryptocurrencies: 1,
-    pages: 0,
+    page: 1,
     hasMore: true,
   };
 
   fetchData = async () => {
-    if (this.state.coinsData.length >= this.state.allCryptocurrencies) {
-      this.setState({ hasMore: false });
-      return;
-    }
-
-    const pages = this.state.pages + 1;
-
     try {
       const { data } = await axios(
-        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currentCurrency}&order=market_cap_desc&per_page=${this.state.numberOfCoinsToLoad}&page=${pages}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
+        `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${this.props.currentCurrency}&order=market_cap_desc&per_page=50&page=${this.state.page}&sparkline=true&price_change_percentage=1h%2C24h%2C7d`
       );
-      const response =
-        pages === 1 && (await axios("https://api.coingecko.com/api/v3/global"));
       this.setState({
         coinsData: this.state.coinsData.concat(data),
         isLoading: false,
-        allCryptocurrencies:
-          pages === 1
-            ? response.data.data.active_cryptocurrencies
-            : this.state.allCryptocurrencies,
-        pages,
+        hasMore: data.length === 50,
       });
     } catch (error) {
       toast.error("Error while loading coins data...", { toastId: "Table" });
       this.setState({ isLoading: false });
     }
   };
+
+  updatePageNumber = () => {
+    console.log("hasRun", this.state.page + 1);
+    this.setState({ page: this.state.page + 1 });
+  };
+
+  componentDidUpdate(_, prevState) {
+    if (this.state.page !== prevState.page) {
+      this.fetchData();
+    }
+  }
 
   componentDidMount() {
     this.fetchData();
@@ -53,6 +49,7 @@ class Table extends React.Component {
 
     return (
       <Container>
+        {console.log(this.state.page, this.state.coinsData.length)}
         <TableSort />
         {haveData ? (
           <InfiniteScroll
@@ -64,8 +61,8 @@ class Table extends React.Component {
               alignItems: "center",
             }}
             dataLength={this.state.coinsData.length}
-            next={this.fetchData}
             hasMore={this.state.hasMore}
+            next={this.updatePageNumber}
             loader={<InfoInfiniteScroll info={"Loading..."} />}
             endMessage={<InfoInfiniteScroll info={"Yay! You have seen it all"} />}
           >
@@ -78,7 +75,7 @@ class Table extends React.Component {
             ))}
           </InfiniteScroll>
         ) : (
-          Array(this.state.numberOfCoinsToLoad)
+          Array(50)
             .fill(0)
             .map((el) => <TableCoinSkeleton key={Math.random()} />)
         )}
