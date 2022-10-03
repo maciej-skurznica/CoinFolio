31a78f6e6a261321ff26d1323dc28b9ccd0af5fb
Maciej Skurznica <maciej.skurznica@gmail.com>
2022-10-03 14:36:38 +0100

    CF-24 navigate to correct coin and fetch data

--- src/pages/Coin/Coin.js
+++ src/pages/Coin/Coin.js
@@ -1,5 +1,40 @@
-const Coin = () => {
-  return "a";
-};
+import React from "react";
+import axios from "axios";
+import { toast } from "react-toastify";
+
+class Coin extends React.Component {
+  state = { isLoading: true, coinData: {} };
+
+  coinId = this.props.match.params.coin;
+
+  fetchData = async () => {
+    try {
+      const { data } = await axios(
+        `https://api.coingecko.com/api/v3/coins/${this.coinId}?localization=false&tickers=false&market_data=true&community_data=true&developer_data=false&sparkline=false`
+      );
+      this.setState({
+        coinData: data,
+        isLoading: false,
+      });
+    } catch (error) {
+      toast.error("Error while loading coin data...", { toastId: "Coin" });
+      this.setState({ isLoading: false });
+    }
+  };
+
+  componentDidUpdate(prevProps, _) {
+    if (this.coinId !== prevProps.match.params.coin) {
+      this.fetchData();
+    }
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    return <>{this.state.coinData?.id}</>;
+  }
+}
 
 export default Coin;
