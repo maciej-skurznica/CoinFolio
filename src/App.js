27145091eeaccd9e94e9af426cd8687f944d8ad1
Maciej Skurznica <maciej.skurznica@gmail.com>
2022-10-07 19:21:33 +0100

    CF-27 App.js refactor to hooks

--- src/App.js
+++ src/App.js
@@ -2,91 +2,69 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { Slide } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+import { SkeletonTheme } from "react-loading-skeleton";
 import { Home, Coin, Portfolio, NotFound } from "pages";
 import { Navbar } from "components";
-import { ThemeProvider } from "styled-components";
-import { SkeletonTheme } from "react-loading-skeleton";
-import { lightTheme, darkTheme } from "styles/themes";
+import { useLocalStorageAndState } from "hooks";
 import { GlobalStyle, StyledToastContainer } from "styles/global";
+import { ThemeProvider } from "styled-components";
+import { lightTheme, darkTheme } from "styles/themes";
 
-class App extends React.Component {
-  state = {
-    darkThemeOn: false,
-    currency: "USD",
-  };
+const App = () => {
+  const [currency, setCurrency] = useLocalStorageAndState("currency", "USD");
+  const [darkThemeOn, setDarkThemeOn] = useLocalStorageAndState("darkThemeOn", false);
 
-  toggleCurrency = (chosenCurrency, handleClick) => {
+  const toggleCurrency = (chosenCurrency, handleClick) => {
     handleClick();
-    this.setLocalStorage("currency", chosenCurrency);
-    this.setState({ currency: chosenCurrency });
+    setCurrency(chosenCurrency);
   };
 
-  toggleTheme = () => {
-    this.setLocalStorage("darkThemeOn", !this.state.darkThemeOn);
-    this.setState({ darkThemeOn: !this.state.darkThemeOn });
-  };
+  const toggleTheme = () => setDarkThemeOn(!darkThemeOn);
 
-  setLocalStorage = (item, itemValue) =>
-    window.localStorage.setItem(item, JSON.stringify(itemValue));
-
-  componentDidMount() {
-    this.setState({
-      darkThemeOn: JSON.parse(window.localStorage.getItem("darkThemeOn")),
-      currency:
-        JSON.parse(window.localStorage.getItem("currency")) || this.state.currency,
-    });
-  }
-
-  render() {
-    return (
-      <ThemeProvider theme={this.state.darkThemeOn ? darkTheme : lightTheme}>
-        <SkeletonTheme
-          baseColor={this.state.darkThemeOn ? "#313131" : "#E9E9E8"}
-          highlightColor={this.state.darkThemeOn ? "#525252" : "#FEFFFE"}
-        >
-          <Router>
-            <GlobalStyle />
-            <Navbar
-              toggleCurrency={this.toggleCurrency}
-              currentCurrency={this.state.currency}
-              toggleTheme={this.toggleTheme}
-              darkThemeOn={this.state.darkThemeOn}
-            />
-            <Switch>
-              <Route
-                exact
-                path="/"
-                component={(props) => (
-                  <Home {...props} currentCurrency={this.state.currency} />
-                )}
-              />
-              <Route
-                exact
-                path="/coins/:coin"
-                render={(props) => (
-                  <Coin {...props} currentCurrency={this.state.currency} />
-                )}
-              />
-              <Route exact path="/portfolio" component={Portfolio} />
-              <Route path="*" component={NotFound} />
-            </Switch>
-          </Router>
-          <StyledToastContainer
-            theme={this.state.darkThemeOn ? "dark" : "light"}
-            transition={Slide}
-            position="bottom-right"
-            autoClose={6000}
-            newestOnTop={false}
-            closeOnClick
-            rtl={false}
-            pauseOnFocusLoss
-            draggable
-            pauseOnHover
+  return (
+    <ThemeProvider theme={darkThemeOn ? darkTheme : lightTheme}>
+      <SkeletonTheme
+        baseColor={darkThemeOn ? "#313131" : "#E9E9E8"}
+        highlightColor={darkThemeOn ? "#525252" : "#FEFFFE"}
+      >
+        <Router>
+          <GlobalStyle />
+          <Navbar
+            toggleCurrency={toggleCurrency}
+            currentCurrency={currency}
+            toggleTheme={toggleTheme}
+            darkThemeOn={darkThemeOn}
           />
-        </SkeletonTheme>
-      </ThemeProvider>
-    );
-  }
-}
+          <Switch>
+            <Route
+              exact
+              path="/"
+              component={(props) => <Home {...props} currentCurrency={currency} />}
+            />
+            <Route
+              exact
+              path="/coins/:coin"
+              render={(props) => <Coin {...props} currentCurrency={currency} />}
+            />
+            <Route exact path="/portfolio" component={Portfolio} />
+            <Route path="*" component={NotFound} />
+          </Switch>
+        </Router>
+        <StyledToastContainer
+          theme={darkThemeOn ? "dark" : "light"}
+          transition={Slide}
+          position="bottom-right"
+          autoClose={6000}
+          newestOnTop={false}
+          closeOnClick
+          rtl={false}
+          pauseOnFocusLoss
+          draggable
+          pauseOnHover
+        />
+      </SkeletonTheme>
+    </ThemeProvider>
+  );
+};
 
 export default App;
---
+++ src/hooks/index.js
@@ -0,0 +1,1 @@
+export { default as useLocalStorageAndState } from "./useLocalStorageAndState";
