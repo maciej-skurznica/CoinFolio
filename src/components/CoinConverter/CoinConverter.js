f492cd684bffa050cca8752c4cdd9fd4f78acf58
Maciej Skurznica <maciej.skurznica@gmail.com>
2022-10-11 23:03:43 +0100

    CoinConverter.js

--- src/components/CoinConverter/CoinConverter.js
+++ src/components/CoinConverter/CoinConverter.js
@@ -1,77 +1,61 @@
-import React from "react";
+import React, { useState } from "react";
 import { CgArrowsExchangeAlt } from "react-icons/cg";
 import { displayBigNumber } from "utils";
 import { Div } from "ui";
 import { Container, ConvertIcon, Left, InputOnRight } from "./CoinConverter.styles";
 
-class CoinConverter extends React.Component {
-  state = {
-    fiatValue: "",
-    fiatPlaceholder: "Type value",
-    cryptoValue: "",
-    cryptoPlaceholder: "Type value",
-  };
+const CoinConverter = ({ coinData, currentCurrency }) => {
+  const [fiatValue, setFiatValue] = useState("");
+  const [fiatPlaceholder, setFiatPlaceholder] = useState("Type value");
+  const [cryptoValue, setCryptoValue] = useState("");
+  const [cryptoPlaceholder, setCryptoPlaceholder] = useState("Type value");
 
-  cryptoPrice =
-    this.props.coinData.market_data.current_price[
-      this.props.currentCurrency.toLowerCase()
-    ];
+  const runConverter = () =>
+    fiatValue.length
+      ? setCryptoPlaceholder(displayBigNumber(fiatValue / cryptoPrice, 8))
+      : setFiatPlaceholder(displayBigNumber(cryptoValue * cryptoPrice));
 
-  runConverter = () => {
-    if (this.state.fiatValue.length) {
-      this.setState({
-        cryptoPlaceholder: displayBigNumber(this.state.fiatValue / this.cryptoPrice, 8),
-      });
+  const handleChange = (e, active) => {
+    if (active === "fiat") {
+      setFiatValue(e.target.value);
+      setCryptoValue("");
+      setCryptoPlaceholder("value...");
     } else {
-      this.setState({
-        fiatPlaceholder: displayBigNumber(this.state.cryptoValue * this.cryptoPrice),
-      });
+      setCryptoValue(e.target.value);
+      setFiatValue("");
+      setFiatPlaceholder("value...");
     }
   };
 
-  handleSubmit = (e) => {
-    e.preventDefault();
-    this.runConverter();
-  };
+  const cryptoPrice = coinData.market_data.current_price[currentCurrency.toLowerCase()];
+  const fiatTicker = currentCurrency;
+  const cryptoTicker = coinData.symbol.toUpperCase();
 
-  handleChange = (e, active) => {
-    this.setState(
-      active === "fiat"
-        ? { fiatValue: e.target.value, cryptoValue: "", cryptoPlaceholder: "value..." }
-        : { cryptoValue: e.target.value, fiatValue: "", fiatPlaceholder: "value..." }
-    );
-  };
-
-  render() {
-    const fiatTicker = this.props.currentCurrency;
-    const cryptoTicker = this.props.coinData.symbol.toUpperCase();
-
-    return (
-      <Container>
-        <Div>
-          <Left>{fiatTicker}</Left>
-          <InputOnRight
-            onChange={(e) => this.handleChange(e, "fiat")}
-            value={this.state.fiatValue}
-            placeholder={this.state.fiatPlaceholder}
-            type="number"
-          />
-        </Div>
-        <ConvertIcon onClick={this.runConverter}>
-          <CgArrowsExchangeAlt />
-        </ConvertIcon>
-        <Div>
-          <Left>{cryptoTicker}</Left>
-          <InputOnRight
-            onChange={(e) => this.handleChange(e, "crypto")}
-            value={this.state.cryptoValue}
-            placeholder={this.state.cryptoPlaceholder}
-            type="number"
-          />
-        </Div>
-      </Container>
-    );
-  }
-}
+  return (
+    <Container>
+      <Div>
+        <Left>{fiatTicker}</Left>
+        <InputOnRight
+          onChange={(e) => handleChange(e, "fiat")}
+          value={fiatValue}
+          placeholder={fiatPlaceholder}
+          type="number"
+        />
+      </Div>
+      <ConvertIcon onClick={runConverter}>
+        <CgArrowsExchangeAlt />
+      </ConvertIcon>
+      <Div>
+        <Left>{cryptoTicker}</Left>
+        <InputOnRight
+          onChange={(e) => handleChange(e, "crypto")}
+          value={cryptoValue}
+          placeholder={cryptoPlaceholder}
+          type="number"
+        />
+      </Div>
+    </Container>
+  );
+};
 
 export default CoinConverter;
