4db02f93dee67b6d6eb2099c3b4a299c0f2116ed
Maciej Skurznica <maciej.skurznica@gmail.com>
2022-10-24 16:12:23 +0100

    CF-6 address requested changes v2

--- src/components/RightTop/RightTop.js
+++ src/components/RightTop/RightTop.js
@@ -4,18 +4,22 @@ import { SunIcon } from "@heroicons/react/24/outline";
 import { Currency } from "components";
 import { toggleTheme } from "store/appSlice";
 import { Container, Divider, StyledLink, Theme } from "./RightTop.styles";
+import { useCallback } from "react";
 
 const RightTop = () => {
   const darkThemeOn = useSelector(({ app }) => app.darkThemeOn);
   const dispatch = useDispatch();
+  const onClickCallback = useCallback(
+    () => dispatch(toggleTheme()),
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+    [dispatch, toggleTheme]
+  );
 
   return (
     <Container>
       <Currency />
       <Divider>|</Divider>
-      <Theme onClick={() => dispatch(toggleTheme())}>
-        {darkThemeOn ? <SunIcon /> : <MoonIcon />}
-      </Theme>
+      <Theme onClick={onClickCallback}>{darkThemeOn ? <SunIcon /> : <MoonIcon />}</Theme>
       <Divider>|</Divider>
       <StyledLink to={"/"}>Sign In</StyledLink>
     </Container>
